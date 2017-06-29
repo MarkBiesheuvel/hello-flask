b6b17a6ec87462c350540101eb9e8eeb10e66960
Mark Biesheuvel <mail@markbiesheuvel.nl>
2017-06-29 11:58:39 +0200

    Add some boto3 stuff

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 from flask import Flask
 import os
 import socket
+import boto3
+
+queue_url = os.getenv("SQS_LOGGING_QUEUE_URL")
 
 app = Flask(__name__)
 
+template = "<h3>Hello Flask (Version 2.2)!</h3>" \
+       "<b>Hostname:</b> {hostname}<br/>" \
+       "<b>Queue Url:</b> {queue_url}<br>" \
+       "<b>Message ID:</b> {message_id}<br>"
+
 @app.route("/")
 def hello():
-    html = "<h3>Hello Flask (Version 2.1)!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>"
-    return html.format(hostname=socket.gethostname())
+    try:
+        client = boto3.client('sqs')
+        response = client.receive_message(
+            QueueUrl=queue_url,
+            MaxNumberOfMessages=1,
+            VisibilityTimeout=5,
+            WaitTimeSeconds=20
+        )
+        message_id = response['Messages'][0]['MessageId'] if len(response['Messages']) > 0 else None
+    except:
+        message_id = None
+
+    return template.format(hostname=socket.gethostname(), queue_url=queue_url, message_id=message_id)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
